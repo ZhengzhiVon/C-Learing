2e06ec70a0c3131b45c1f1566bf47895c720024c
ZhengzhiVon <1455612471@qq.com>
2023-05-26 16:56:20 +0800

    rvalue

--- code/左值右值.cpp
+++ code/左值右值.cpp
@@ -150,3 +150,112 @@ int main()
     return 0;
 }
 */
+
+//类类型的右值引用
+int main1()
+{
+    Int a=Int(1);
+    //Int& b=Int(2);
+    const Int& c=Int(3);
+    Int&& d=Int(4);
+    a.ShowValue();
+    c.ShowValue();
+    d.ShowValue();
+    return 0;
+}
+
+//构造函数的隐式转换
+int main2()
+{
+    const Int &a=1;
+    //const int &a=(Int)(1);
+    Int && b=2;
+    //Int && b=(Int)(2);
+    Int && c=b; //error
+    Int &d=b;
+    return 0;
+}
+
+//使用explicit明确关键，组织构造函数的隐式转换
+/*
+class Int
+{
+    int _value;
+    public:
+    explicit Int(int x=0) :_value(x)
+    {
+        cout<<"Create Int: "<<_value<<endl;
+    }
+};*/
+
+//右值引用与函数重载
+Int func()
+{
+    Int tmp(10);
+    return tmp;
+}
+
+int main()
+{
+    Int a=func();
+    Int& b=func();
+    const Int& C=func();
+    Int&& rd=func();
+    return 0;
+}
+
+Int func(int x)
+{
+    Int tmp(x);
+    return tmp;
+}
+
+int main4()
+{
+    Int a = func(1);//优化：return的tmp直接构建了a本身，省略构建将亡值对象
+    /*
+    Int a;
+    a=func();	//这样先构建一个将亡值对象tmp，在构建一个对象a，然后把tmp赋值给a
+    */
+    //Int& b = func(2);//error,返回一个将亡值对象(右值)
+    const Int& c = func(3);//const固定将亡值对象，直到主函数结束时在销毁释放
+    Int&& rd = func(4); //再再主函数栈帧中固定一个将亡值对象
+    return 0;
+}
+/*
+Int &func(int x)
+{
+    Int tmp(x);
+    return tmp;//以引用&返回，不构建一个将亡值对象，直接将tmp返回给所接受的对象，func函数结束后tmp对象就直接被销毁
+}
+
+int main()
+{
+    Int a = func(1);//用tmp初始化a之后，tnp已经被销毁
+    //Int a=*func(1);	实际执行了解引用
+    
+    //b、c引用的是一个已经被销毁释放的对象
+    Int& b = func(2);//
+    const Int& c = func(3);
+    //Int&& rd = func(4);//error，以&返回则为左值不能以右值引用接收 
+    return 0;
+}
+*/
+
+//以右值引用返回
+/*
+Int&& func(int x)
+{
+	//Int tmp(x);
+    //return tmp;//error,tmp是一个左值，不能以右值引用接收
+    return Int(x);//ok,不具名对象为右值，可以被右值引用接收。此不具名对象构建在func()函数的栈帧之中，不在主函数的栈帧中
+    
+}
+
+int main()
+{
+    Int a=func(1);
+    a.ShowValue();//fun函数结束之后，已经被销毁
+    return 0;
+}
+*/
